24d95dbf35ad9523a8eeacbd64d79d3b25c5575a
Alexander <sebetelist@protonmail.com>
2026-08-08 23:45:54 +0100

    Change name to validate_character, add the general Character class

--- rpg_character.py
+++ rpg_character.py
@@ -5,68 +5,72 @@ empty_dot = "○"
 MAX_STAT = 10
 
 
-def create_character(name, strength, intelligence, charizma):
+class Character:
+    def __init__(self, name, strength, intelligence, charizma):
+        self.name = name
+        self.strength = strength
+        self.intelligence = intelligence
+        self.charizma = charizma
 
-    if not isinstance(name, str):
-        return "The character name should be a string"
-    elif name == "":
-        return "The character should have a name"
-    elif len(name) > 10:
-        return "The character name is too long"
-    elif " " in name:
-        return "The character name should not contain spaces"
+    def choose_class(self):
+        class_descriptions = {
+            "Warrior": "A fearless fighter, strong in battle.",
+            "Mage": "A master of arcane knowledge.",
+            "Bard": "A charismatic storyteller and performer.",
+        }
+        if self.strength >= self.intelligence and self.strength >= self.charizma:
+            char_class = "Warrior"
+        elif self.intelligence >= self.charizma:
+            char_class = "Mage"
+        else:
+            char_class = "Bard"
+        return char_class, class_descriptions.get(char_class)
 
-    if (
-        not isinstance(strength, int)
-        or not isinstance(intelligence, int)
-        or not isinstance(charizma, int)
-    ):
-        return "All stats should be integers"
-    elif strength < 1 or intelligence < 1 or charizma < 1:
-        return "All stats should be no less than 1"
-    elif strength > 4 or intelligence > 4 or charizma > 4:
-        return "All stats should be no more than 4"
-    elif sum([strength, intelligence, charizma]) != 7:
-        return "The character should start with 7 points"
-    character_stats = stats(strength, intelligence, charizma)
-    hero_class, hero_description = choose_class(strength, intelligence, charizma)
+    def stats(self):
+        stat_string = empty_dot * MAX_STAT
+        strn = stat_string.replace(empty_dot, full_dot, self.strength)
+        intl = stat_string.replace(empty_dot, full_dot, self.intelligence)
+        char = stat_string.replace(empty_dot, full_dot, self.charizma)
 
-    return f"{name}\n{hero_class}: {hero_description}\n{character_stats}"
+        return f"STR {strn}\nINT {intl}\nCHA {char}"
 
+    def validate_character(self):
 
-def stats(strength, intelligence, charizma):
-    stat_string = empty_dot * MAX_STAT
-    strn = stat_string.replace(empty_dot, full_dot, strength)
-    intl = stat_string.replace(empty_dot, full_dot, intelligence)
-    char = stat_string.replace(empty_dot, full_dot, charizma)
+        if not isinstance(self.name, str):
+            return "The character name should be a string"
+        elif self.name == "":
+            return "The character should have a name"
+        elif len(self.name) > 10:
+            return "The character name is too long"
+        elif " " in self.name:
+            return "The character name should not contain spaces"
 
-    return f"STR {strn}\nINT {intl}\nCHA {char}"
+        if (
+            not isinstance(self.strength, int)
+            or not isinstance(self.intelligence, int)
+            or not isinstance(self.charizma, int)
+        ):
+            return "All stats should be integers"
+        elif self.strength < 1 or self.intelligence < 1 or self.charizma < 1:
+            return "All stats should be no less than 1"
+        elif self.strength > 4 or self.intelligence > 4 or self.charizma > 4:
+            return "All stats should be no more than 4"
+        elif sum([self.strength, self.intelligence, self.charizma]) != 7:
+            return "The character should start with 7 points"
+        character_stats = self.stats()
+        hero_class, hero_description = self.choose_class()
 
-
-def choose_class(strength, intelligence, charizma):
-    class_descriptions = {
-        "Warrior": "A fearless fighter, strong in battle.",
-        "Mage": "A master of arcane knowledge.",
-        "Bard": "A charismatic storyteller and performer.",
-    }
-    if strength >= intelligence and strength >= charizma:
-        char_class = "Warrior"
-    elif intelligence >= charizma:
-        char_class = "Mage"
-    else:
-        char_class = "Bard"
-    return char_class, class_descriptions.get(char_class)
+        return f"{self.name}\n{hero_class}: {hero_description}\n{character_stats}"
 
 
 try:
-    print(
-        create_character(
-            input("Hero name: "),
-            int(input("STR: ")),
-            int(input("INT: ")),
-            int(input("CHA: ")),
-        )
+    hero = Character(
+        input("Hero name: "),
+        int(input("STR: ")),
+        int(input("INT: ")),
+        int(input("CHA: ")),
     )
+    print(hero.validate_character())
 except KeyboardInterrupt:
     print("\nExit")
 except ValueError:
